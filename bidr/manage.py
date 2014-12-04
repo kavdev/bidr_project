14e7cda3cfe8284e28f2802ae8a9b086ba25ccd6
Alexander Kavanaugh <kavanaugh.development@outlook.com>
2014-12-04 01:10:41 -0800

    took out env for production

--- bidr/manage.py
+++ bidr/manage.py
@@ -55,7 +55,8 @@ if __name__ == "__main__":
     # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.local")
 
     color_init()
-    activate_env()
+    # Enable this in local dev
+    # activate_env()
 
     try:
         from django.core.management import execute_from_command_line
