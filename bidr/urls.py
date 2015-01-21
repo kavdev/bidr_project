e3adf1716ab7f281a1bedd2df512ff3b6b2904a1
Alexander Kavanaugh <kavanaugh.development@outlook.com>
2015-01-21 13:24:07 -0800

    added honeypot

--- bidr/urls.py
+++ bidr/urls.py
@@ -11,7 +11,7 @@ import logging
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.staticfiles.templatetags.staticfiles import static
-from django.views.generic import RedirectView
+from django.views.generic.base import RedirectView, TemplateView
 from django.views.defaults import permission_denied, page_not_found
 
 from rest_framework.routers import DefaultRouter
@@ -39,6 +39,7 @@ urlpatterns = [
     url(r'^favicon\.ico$', RedirectView.as_view(url=static('img/favicon.ico')), name='favicon'),
     url(r'^robots\.txt$', RedirectView.as_view(url=static('robots.txt')), name='robots'),
     url(r'^flugzeug/', include(admin.site.urls)),  # admin site urls, masked
+    url(r'^admin$', TemplateView.as_view(template_name="honeypot.html"), name="contact"),  # admin site urls, honeypot
     url(r'^api/auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/token-auth/', views.obtain_auth_token),
     url(r'^api/', include(bidruser_router.urls)),
