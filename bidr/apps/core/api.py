7f630fb476e0d0e731e55a94571ebd5979c60734
glazed4444 <glazed4@yahoo.com>
2015-02-04 21:57:22 -0800

    Debug code for register endpoint.

--- bidr/apps/core/api.py
+++ bidr/apps/core/api.py
@@ -6,6 +6,8 @@
 
 """
 
+import logging
+
 from django.contrib.auth import get_user_model
 
 from rest_framework import status
@@ -16,6 +18,8 @@ from rest_framework.views import APIView
 
 from .models import BidrUser
 from .serializers import BidrUserSerializer, RegisterBidrUserSerializer
+
+logger = logging.getLogger(__name__)
 
 
 class BidrUserViewSet(ModelViewSet):
@@ -29,14 +33,20 @@ class RegisterBidrUser(APIView):
 
     def post(self, request):
         """Registration code inspired by http://stackoverflow.com/a/19337404."""
-        print(request.data)
+        logger.info(request.data)
         serializer = RegisterBidrUserSerializer(data=request.data)
+        logger.infor("here1")
         valid_fields = [field.name for field in get_user_model()._meta.fields]
+        logger.infor("here2")
 
         if serializer.is_valid(raise_exception=True):
+            logger.infor("here3")
             user_data = {field: data for (field, data) in request.data.items() if field in valid_fields}
+            logger.infor("here4")
             user = get_user_model().objects.create_user(**user_data)
+            logger.infor("here5")
             token = Token.objects.create(user=user)
+            logger.infor("here6")
             return Response({'token': token.key}, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer._errors, status=status.HTTP_400_BAD_REQUEST)
