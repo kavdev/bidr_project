d89a53b75c8ccb33cb7c1996c1a4c8865a238ddc
glazed4444 <glazed4@yahoo.com>
2015-02-04 22:23:53 -0800

    Debuging endpoint.

--- bidr/apps/core/api.py
+++ bidr/apps/core/api.py
@@ -33,20 +33,21 @@ class RegisterBidrUser(APIView):
 
     def post(self, request):
         """Registration code inspired by http://stackoverflow.com/a/19337404."""
-        logger.info(request.data)
+        logger.warning("here0")
+        logger.warning(request.data)
         serializer = RegisterBidrUserSerializer(data=request.data)
-        logger.infor("here1")
+        logger.warning("here1")
         valid_fields = [field.name for field in get_user_model()._meta.fields]
-        logger.infor("here2")
+        logger.warning("here2")
 
         if serializer.is_valid(raise_exception=True):
-            logger.infor("here3")
+            logger.warning("here3")
             user_data = {field: data for (field, data) in request.data.items() if field in valid_fields}
-            logger.infor("here4")
+            logger.warning("here4")
             user = get_user_model().objects.create_user(**user_data)
-            logger.infor("here5")
+            logger.warning("here5")
             token = Token.objects.create(user=user)
-            logger.infor("here6")
+            logger.warning("here6")
             return Response({'token': token.key}, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer._errors, status=status.HTTP_400_BAD_REQUEST)
