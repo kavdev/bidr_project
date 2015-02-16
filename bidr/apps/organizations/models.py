e6ed039927166d930da1ad362c6a96578a19f65b
jdilanch <jdilanch@calpoly.edu>
2015-02-15 20:27:56 -0800

    Create organization works

--- bidr/apps/organizations/models.py
+++ bidr/apps/organizations/models.py
@@ -29,19 +29,19 @@ class Organization(Model):
     phone_number = PhoneNumberField(verbose_name='Phone Number')
     website = URLField(verbose_name="Website")
     owner = ForeignKey(settings.AUTH_USER_MODEL, related_name="owner", verbose_name="Owner")
-    managers = ManyToManyField(settings.AUTH_USER_MODEL, related_name="managers", verbose_name="Managers")
-    auctions = ManyToManyField(Auction, related_name="auctions", verbose_name="Auctions")
-
+    managers = ManyToManyField(settings.AUTH_USER_MODEL, related_name="managers", verbose_name="Managers", blank=True)
+    auctions = ManyToManyField(Auction, related_name="auctions", verbose_name="Auctions", blank=True)
+ 
     def save(self):
         """Auto-populate an empty slug field from the MyModel name and
         if it conflicts with an existing slug then append a number and try
         saving again.
         https://djangosnippets.org/snippets/761/
         """
-
+         
         if not self.slug:
             self.slug = slugify(self.name)  # Where self.name is the field used for 'pre-populate from'
-
+  
         while True:
             try:
                 super(Organization, self).save()
@@ -55,6 +55,6 @@ class Organization(Model):
                     self.slug += '-2'
             else:
                 break
-
+  
     def __str__(self):
         return self.name
