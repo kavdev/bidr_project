9e7cd5026166c62ad5906f75fad7907cff9b88bd
jartelfox@mac.com <jartelfox@mac.com>
2015-02-16 18:06:43 -0800

    adding item details ****** BROKEN RIGHT NOW

---
+++ bidr/apps/items/views.py
@@ -0,0 +1,15 @@
+"""
+.. module:: bidr.apps.items.models
+   :synopsis: Bidr Silent Auction System Item Models.
+
+.. moduleauthor:: Jarred Stelfox <sstelfox@calpoly.edu>
+
+"""
+from django.views.generic.detail import DetailView
+from .models import AbstractItem
+
+class ItemDetailView(DetailView):
+    model = AbstractItem
+    
+    def get_object(self, queryset=None):
+        return AbstractItem.objects.get(bidables__id=self.kwargs['auction_id'], bidables__auctions__slug=self.kwargs['slug'], id=self.kwargs['item_id'])
