87612da16ed201b14326cc142b8d9678b003c5ea
Alexander Kavanaugh <kavanaugh.development@outlook.com>
2015-02-19 12:40:41 -0800

    switched to dj-static

--- bidr/wsgi.py
+++ bidr/wsgi.py
@@ -1,9 +1,10 @@
 import django
 from django.core.wsgi import get_wsgi_application
 from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
-from whitenoise.django import DjangoWhiteNoise
+# from whitenoise.django import DjangoWhiteNoise
+from dj_static import Cling, MediaCling
 
 django.setup()
 
 # Send any wsgi errors to Sentry
-application = Sentry(DjangoWhiteNoise(get_wsgi_application()))
+application = Sentry(Cling(MediaCling(get_wsgi_application())))
