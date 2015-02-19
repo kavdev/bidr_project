e22a4b87b90d59dbc5c5b5db8c528c0041408b9d
Alexander Kavanaugh <kavanaugh.development@outlook.com>
2015-02-18 21:20:41 -0800

    item model fix

--- bidr/apps/items/models.py
+++ bidr/apps/items/models.py
@@ -15,20 +15,21 @@ from polymorphic import PolymorphicModel
 
 from ..bids.models import Bid
 
+
 class AbstractItem(PolymorphicModel):
     """ Abstract class for Items """
-    
+
     name = CharField(max_length=60)
-    description = TextField()
+    description = TextField(blank=True)
     claimed = BooleanField(default=False)
     claimed_bid = ForeignKey(Bid, null=True, blank=True, verbose_name="Claimed Bid")
     bids = ManyToManyField(Bid, blank=True, related_name="bids", verbose_name="Bids")
-    
+
     def claim(self, bid):
         self.claimed = True
         self.claimed_bid = bid
         self.save()
-    
+
     def __str__(self):
         return self.name
 
@@ -36,8 +37,8 @@ class AbstractItem(PolymorphicModel):
 class Item(AbstractItem):
     """ An auction item."""
 
-    min_price = DecimalField(max_digits=7, decimal_places=2)
-    picture = ImageField()
+    min_price = DecimalField(max_digits=7, decimal_places=2, default=0)
+    picture = ImageField(null=True, blank=True)
 
     tags = TaggableManager()
 
@@ -46,8 +47,8 @@ class ItemCollection(AbstractItem):
     """ A collection of items."""
 
     items = ManyToManyField(Item, verbose_name="Items")
-    
+
     def claim(self, bid):
         for item in self.items.all():
             item.claim(bid)
-        super(ItemCollection, self).claim(bid)    
+        super(ItemCollection, self).claim(bid)
