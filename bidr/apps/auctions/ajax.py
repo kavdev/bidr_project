23303a5f635103fbc152514256414337e04406eb
Alexander Kavanaugh <kavanaugh.development@outlook.com>
2015-02-19 00:21:48 -0800

    ajax fix

--- bidr/apps/auctions/ajax.py
+++ bidr/apps/auctions/ajax.py
@@ -10,13 +10,16 @@ from django.views.decorators.http import require_POST
 
 from django_ajax.decorators import ajax
 
-from bidr.apps.items.models import Item
+from ..bids.models import Bid
+from ..items.models import Item
 
 
 @ajax
 @require_POST
-def claim_item(request):
+def claim_item(request, slug, auction_id):
     item_id = request.POST["item_id"]
+    bid_id = request.POST["bid_id"]
 
     item_instance = Item.objects.get(id=item_id)
-    item_instance.claim()
+    bid_instance = Bid.objects.get(id=bid_id)
+    item_instance.claim(bid_instance)
