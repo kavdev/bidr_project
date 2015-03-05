1414c5081c84d93af9a0916859cd92ac8a249c04
Alexander Kavanaugh <kavanaugh.development@outlook.com>
2015-03-04 17:16:16 -0800

    began adding item collections

--- bidr/apps/items/ajax.py
+++ bidr/apps/items/ajax.py
@@ -11,7 +11,7 @@ from django.views.decorators.http import require_POST
 from django_ajax.decorators import ajax
 
 from ..bids.models import Bid
-from ..items.models import Item
+from ..items.models import Item, ItemCollection
 
 
 @ajax
@@ -32,3 +32,23 @@ def delete_item(request, slug, auction_id):
 
     item_instance = Item.objects.get(id=item_id)
     item_instance.delete()
+
+
+@ajax
+@require_POST
+def delete_item_collection(request, slug, auction_id):
+    itemcollection_id = request.POST["itemcollection_id"]
+
+    item_collection_instance = ItemCollection.objects.get(id=itemcollection_id)
+    item_collection_instance.delete()
+
+
+@ajax
+@require_POST
+def remove_item_from_collection(request, slug, auction_id):
+    item_id = request.POST["item_id"]
+    itemcollection_id = request.POST["itemcollection_id"]
+
+    item_collection_instance = ItemCollection.objects.get(id=itemcollection_id)
+    item_collection_instance.items.remove(item_id)
+    item_collection_instance.save()
