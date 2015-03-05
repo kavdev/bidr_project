b8fdde21b6f97ebde3daae134d363d764a337600
Zachary Glazer <glazed4@yahoo.com>
2015-03-05 02:28:30 -0800

    Fixing merge conflict

--- bidr/urls.py
+++ bidr/urls.py
@@ -25,11 +25,12 @@ from .apps.bids.api import BidViewSet
 from .apps.core.api import BidrUserViewSet
 
 from .apps.auctions.views import AuctionView, AuctionCreateView, AuctionUpdateView, AuctionPlanView, AuctionObserveView, AuctionClaimView, AuctionReportView, start_auction, end_auction
+from .apps.auctions.api import AddAuctionParticipantView
 from .apps.core.views import IndexView, LoginView, logout, handler500
 from .apps.organizations.views import OrganizationListView, OrganizationCreateView
 from .apps.items.views import ItemCreateView, ItemCollectionCreateView
 
-from .apps.items.ajax import claim_item, delete_item, remove_item_from_collection, delete_item_collection
+from .apps.items.ajax import claim_item, delete_item, add_item_to_collection, remove_item_from_collection, delete_item_collection
 
 from .apps.core.utils import user_is_type, UserType
 
@@ -52,27 +53,25 @@ urlpatterns = [
     url(r'^robots\.txt$', RedirectView.as_view(url=staticfiles('robots.txt')), name='robots'),
     url(r'^flugzeug/', include(admin.site.urls)),  # admin site urls, masked
     url(r'^admin/$', TemplateView.as_view(template_name="honeypot.html"), name="contact"),  # admin site urls, honeypot
-    url(r'^api/auth/', include('rest_framework.urls', namespace='rest_framework')),
-#     url(r'^api/token-auth/$', views.obtain_auth_token),
-    url(r'^api/', include(bidruser_router.urls)),
     url(r'^login/$', LoginView.as_view(), name='admin_login'),
     url(r'^logout/$', logout, name='admin_logout'),
 ]
 
-# Bids
+# API
 urlpatterns += [
     url(r'^api/', include(bid_router.urls)),
-]
-
-# Registration
-urlpatterns += [
-    url(r'^api/', include(api_auth_urls))
+    url(r'^api/auth/', include(api_auth_urls)),
+#    url(r'^api/auth/', include('rest_framework.urls', namespace='rest_framework')),
+#    url(r'^api/token-auth/$', views.obtain_auth_token),
+    url(r'^api/', include(bidruser_router.urls)),
+    url(r'^api/auctions/(?P<pk>\d+)/add-participant/', AddAuctionParticipantView.as_view()),
 ]
 
 # Organizations
 urlpatterns += [
     url(r'^organizations/$', login_required(OrganizationListView.as_view()), name="organizations"),
     url(r'^organizations/create/$', login_required(OrganizationCreateView.as_view()), name="create_organization"),
+#     url(r'^organizations/update/(?P<slug>[\w-]+)/$', login_required(OrganizationUpdateView.as_view()), name="update_organization"),
 ]
 
 # Auctions
@@ -93,6 +92,7 @@ urlpatterns += [
     url(r'^organizations/(?P<slug>[\w-]+)/auctions/(?P<auction_id>\d+)/items/create/$', login_required(user_is_type(UserType.MANAGER)(ItemCreateView.as_view())), name='create_item'),
     url(r'^organizations/(?P<slug>[\w-]+)/auctions/(?P<auction_id>\d+)/items/delete/$', login_required(user_is_type(UserType.MANAGER)(delete_item)), name='delete_item'),
     url(r'^organizations/(?P<slug>[\w-]+)/auctions/(?P<auction_id>\d+)/itemcollections/create/$', login_required(user_is_type(UserType.MANAGER)(ItemCollectionCreateView.as_view())), name='create_item_collection'),
+    url(r'^organizations/(?P<slug>[\w-]+)/auctions/(?P<auction_id>\d+)/itemcollections/add-item/$', login_required(user_is_type(UserType.MANAGER)(add_item_to_collection)), name='add_item_to_collection'),
     url(r'^organizations/(?P<slug>[\w-]+)/auctions/(?P<auction_id>\d+)/itemcollections/remove-item/$', login_required(user_is_type(UserType.MANAGER)(remove_item_from_collection)), name='remove_item_from_collection'),
     url(r'^organizations/(?P<slug>[\w-]+)/auctions/(?P<auction_id>\d+)/itemcollections/delete/$', login_required(user_is_type(UserType.MANAGER)(delete_item_collection)), name='delete_item_collection'),
     url(r'^organizations/(?P<slug>[\w-]+)/auctions/(?P<auction_id>\d+)/claim/claim-item/$', login_required(user_is_type(UserType.MANAGER)(claim_item)), name='claim_item'),
