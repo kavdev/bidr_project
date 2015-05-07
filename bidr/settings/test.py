0e61a1c74c1f3de54e6497dcfce7f4e7397df480
Alexander Kavanaugh <kavanaugh.development@outlook.com>
2015-05-07 03:57:36 -0700

    test settings and requirements fix

--- bidr/settings/test.py
+++ bidr/settings/test.py
@@ -1,3 +1,12 @@
+"""
+Test Settings
+
+http://www.daveoncode.com/2013/09/23/effective-tdd-tricks-to-speed-up-django-tests-up-to-10x-faster/
+"""
+
+
+import logging
+
 from .base import *  # noqa
 
 DATABASES = {
@@ -7,6 +16,41 @@ DATABASES = {
     }
 }
 
-# INSTALLED_APPS += ("django_nose",)
-#
-# TEST_RUNNER = "django_nose.NoseTestSuiteRunner"
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'polymorphic',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.admin',
+    'django.contrib.staticfiles',
+    'django_ajax',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+    'taggit',
+    'bidr.apps.auctions',
+    'bidr.apps.core',
+    'bidr.apps.core.templatetags.CoreTemplatetagsConfig',
+    'bidr.apps.datatables',
+    'bidr.apps.datatables.templatetags.DatatablesTemplatetagsConfig',
+    'bidr.apps.bids',
+    'bidr.apps.items',
+    'bidr.apps.organizations',
+)
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+logging.disable(logging.CRITICAL)
