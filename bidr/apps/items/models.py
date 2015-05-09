c8606626b360035447b841f8e0673fd06c7b53b5
Alexander Kavanaugh <kavanaugh.development@outlook.com>
2015-05-09 16:29:41 -0700

    addded tags aggregate method to item model, added 100% item model tests

--- bidr/apps/items/models.py
+++ bidr/apps/items/models.py
@@ -35,7 +35,7 @@ class AbstractItem(PolymorphicModel):
         self.save()
 
     def get_second_highest_bid(self):
-        bid_list = self.bids.all().order_by("-amount")
+        bid_list = self.get_bids_by_amount()
         if bid_list.count() > 1:
             return bid_list[1]
         else:
@@ -97,10 +97,11 @@ class ItemCollection(AbstractItem):
 
     items = ManyToManyField(Item, blank=True, verbose_name="Items")
 
-    def claim(self, bid):
-        for item in self.items.all():
-            item.claim(bid)
-        super(ItemCollection, self).claim(bid)
+# @glazed4444 Not sure why this is here. Items are no longer biddables once they're in a collection
+#     def claim(self, bid):
+#         for item in self.items.all():
+#             item.claim(bid)
+#         super(ItemCollection, self).claim(bid)
 
     @property
     def image_urls(self):
@@ -113,3 +114,11 @@ class ItemCollection(AbstractItem):
     @property
     def ordered_items(self):
         return self.items.all().order_by("name")
+
+    @property
+    def tags(self):
+        tag_set = set()
+        # List compression syntax is not python's strongsuit...
+        [tag_set.add(tag) for item in self.items.all() for tag in item.tags.names()]
+
+        return list(tag_set)
