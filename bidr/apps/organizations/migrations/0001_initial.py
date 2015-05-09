3ed3e6f4f71c340229ff160f693e2d722730b773
Alexander Kavanaugh <kavanaugh.development@outlook.com>
2015-05-09 02:01:50 -0700

    fixed migrations

--- bidr/apps/organizations/migrations/0001_initial.py
+++ bidr/apps/organizations/migrations/0001_initial.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 from django.conf import settings
-import phonenumber_field.modelfields
 
 
 class Migration(migrations.Migration):
@@ -20,7 +19,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
                 ('name', models.CharField(max_length=100, verbose_name='Name')),
                 ('email', models.EmailField(max_length=75, blank=True, verbose_name='Email Address', unique=True)),
-                ('phone_number', phonenumber_field.modelfields.PhoneNumberField(max_length=128, verbose_name='Phone Number')),
+                ('phone_number', models.CharField(verbose_name='Phone Number', max_length=20)),
                 ('website', models.URLField(verbose_name='Website')),
                 ('auctions', models.ManyToManyField(to='auctions.Auction')),
                 ('managers', models.ManyToManyField(verbose_name='Manager', to=settings.AUTH_USER_MODEL, related_name='managers')),
